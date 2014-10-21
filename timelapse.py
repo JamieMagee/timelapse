988a1bffb02de4d9bbcc27292454c416082b382c
Jamie Magee <jamie.magee@gmail.com>
2014-10-21 20:48:29 +0200

    Change from Copenhagen (testing) data to Cambridge (real) data. Fix bug with timing logic

--- timelapse.py
+++ timelapse.py
@@ -6,20 +6,18 @@ import os
 sun = ephem.Sun()
 
 Cambridge = ephem.Observer()
-Cambridge.lat = '55.676111'
-Cambridge.lon = '12.568333'
+Cambridge.lat = '52.205'
+Cambridge.lon = '0.119'
+Cambridge.elev = 6
 Cambridge.date = datetime.datetime.now()
 
 sunrise = ephem.localtime(Cambridge.next_rising(sun))
 sunset = ephem.localtime(Cambridge.next_setting(sun))
 
 while True:
-    while sunrise < datetime.datetime.now() < sunset:
-        print('Its the daytime!')
-        print('Take a photo!')
+    while sunrise < datetime.datetime.now() < sunset or datetime.datetime.now() < sunset < sunrise:
         os.system('raspistill -o {}.jpg'.format(datetime.datetime.now().strftime('%y-%m-%d %H:%M')))
         time.sleep(1800)
-    print('Its nighttime!')
     Cambridge.date = datetime.datetime.now()
 
     sunrise = ephem.localtime(Cambridge.next_rising(sun))
